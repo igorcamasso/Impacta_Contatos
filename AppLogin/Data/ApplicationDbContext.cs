efc000698c9fcdaf412458eae741fdd5593490da
Igor Vasconcelos Camasso <igor.camasso@hotmail.com>
2022-11-29 16:51:10 -0300

    Add project files.

---
+++ AppLogin/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AppLogin.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppLogin.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Evento> Eventos { get; set; }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
